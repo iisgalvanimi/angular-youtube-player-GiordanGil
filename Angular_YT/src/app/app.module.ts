ef39cf0acb20684c699fc97374fa7406d0f018dd
GiordanGil <gilerlozano.giordan@iisgalvanimi.edu.it>
2022-02-05 12:59:14 +0000

    Progetto Completato Ma Non Funzionante

--- Angular_YT/src/app/app.module.ts
+++ Angular_YT/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { YouTubePlayerModule } from "@angular/youtube-player";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule , YouTubePlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
